7ab860bbb6d36a42bfb79434851fde2e251f291b
Trung Do <dothanhtrung@pm.me>
2024-06-12 02:11:56 +0700

    Support embedded-hal v0.2 for i2c (#3)
    Provide compatible with embedded-hal v0.2 through feature "hal_0_2".

--- src/i2c.rs
+++ src/i2c.rs
@@ -2,7 +2,8 @@
 //!
 //! [`embedded-hal`]: https://docs.rs/embedded-hal
 
-/// Macro that adds HAL support for a given I2C bindings.
+/// Macro that adds HAL 1.0 support for a given I2C bindings.
+#[cfg(feature = "hal_1_0")]
 #[macro_export]
 macro_rules! add_i2c_hal {
     ($i2c:ident) => {
@@ -96,6 +97,91 @@ macro_rules! add_i2c_hal {
                 &mut self,
                 address: u8,
                 operations: &mut [HalOperation<'_>],
+            ) -> Result<(), Self::Error> {
+                let opers = operations
+                    .iter_mut()
+                    .map(|a| match a {
+                        HalOperation::Read(r) => Operation::Read(r.len().try_into().unwrap()),
+                        HalOperation::Write(w) => Operation::Write(w.to_vec()),
+                    })
+                    .collect::<Vec<Operation>>();
+
+                let _ = Self::transaction(self, u16::from(address), &opers);
+
+                Ok(())
+            }
+        }
+    };
+}
+
+/// Macro that adds HAL 0.2 support for a given I2C bindings.
+#[cfg(feature = "hal_0_2")]
+#[macro_export]
+macro_rules! add_i2c_hal {
+    ($i2c:ident) => {
+        use embedded_hal::blocking::i2c::Operation as HalOperation;
+        use $i2c::{ErrorCode, I2c, NoAcknowledgeSource, Operation};
+
+        #[derive(Debug)]
+        pub struct I2CError {
+            err: ErrorCode,
+        }
+
+        impl embedded_hal::blocking::i2c::Read for I2c {
+            type Error = I2CError;
+
+            fn read(&mut self, address: u8, read: &mut [u8]) -> Result<(), Self::Error> {
+                let data = Self::read(&self, u16::from(address), read.len().try_into().unwrap())
+                    .map_err(|e| I2CError { err: e })?;
+
+                for (read, data) in read.iter_mut().zip(data) {
+                    *read = data;
+                }
+
+                Ok(())
+            }
+        }
+
+        impl embedded_hal::blocking::i2c::Write for I2c {
+            type Error = I2CError;
+
+            fn write(&mut self, address: u8, write: &[u8]) -> Result<(), Self::Error> {
+                Self::write(&self, u16::from(address), write).map_err(|e| I2CError { err: e })
+            }
+        }
+
+        impl embedded_hal::blocking::i2c::WriteRead for I2c {
+            type Error = I2CError;
+
+            fn write_read(
+                &mut self,
+                address: u8,
+                write: &[u8],
+                read: &mut [u8],
+            ) -> Result<(), Self::Error> {
+                let data = Self::write_read(
+                    &self,
+                    u16::from(address),
+                    write,
+                    read.len().try_into().unwrap(),
+                )
+                .map_err(|e| I2CError { err: e })?;
+
+                for (read, data) in read.iter_mut().zip(data) {
+                    *read = data;
+                }
+
+                Ok(())
+            }
+        }
+
+        impl embedded_hal::blocking::i2c::Transactional for I2c {
+            type Error = I2CError;
+
+            fn exec<'a>(
+                &mut self,
+                address: u8,
+                operations: &mut [HalOperation<'a>],
             ) -> Result<(), Self::Error> {
                 let opers = operations
                     .iter_mut()
